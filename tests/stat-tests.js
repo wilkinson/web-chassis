ef439782f86f22b2bf13205e1643e256b83712a1
sean@mathbiol.org <sean@mathbiol.org>
2011-08-17 02:24:49 -0500

    Began a "stat" module as well, although it will take me some time to gather a lot of the codes I have already written and clean them up for release ...

---
+++ tests/stat-tests.js
@@ -0,0 +1,30 @@
+//- JavaScript source code
+
+//- stat-tests.js ~~
+//                                                      ~~ (c) SRW, 17 Aug 2011
+
+chassis(function (q) {
+    "use strict";
+
+    q.lib("base") || q.die();
+    q.lib("stat") || q.die();
+    q.lib("test") || q.die();
+
+    q.test$run("stat", [
+        {
+            args:   [0],
+            answer: "0.5",
+            func:   q.stat$pnorm,
+            name:   "pnorm: single number"
+        },
+        {
+            args:   [[-1, 1]],
+            answer: "0.15865525393145702,0.841344746068543",
+            func:   q.stat$pnorm,
+            name:   "pnorm: array of numbers"
+        }
+    ]);
+
+});
+
+//- vim:set syntax=javascript:
